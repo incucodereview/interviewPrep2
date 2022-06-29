3e35a386785f180eaae99dfc32905b782b66c45e
LeeJongbokZ <s2feeling@naver.com>
2022-06-29 16:28:13 +0900

    #1 Controller, Service, Repository 클래스와 테스트 작성

---
+++ src/main/java/com/example/interviewPrep/quiz/repository/QuizRepository.java
@@ -0,0 +1,11 @@
+package com.example.interviewPrep.quiz.repository;
+
+import com.example.interviewPrep.quiz.dto.ProblemDTO;
+
+import java.util.List;
+
+public interface QuizRepository {
+
+    List<ProblemDTO> init();
+
+}

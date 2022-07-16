44be1324063237216a8902c5aff3d054d2c8d992
injae7034 <injae7034@gmail.com>
2022-07-17 08:49:22 +0900

    다대일 양방향 관계를 다대일 단방향으로 바꿈

--- src/main/java/com/example/interviewPrep/quiz/models/Question.java
+++ src/main/java/com/example/interviewPrep/quiz/models/Question.java
@@ -2,14 +2,10 @@ package com.example.interviewPrep.quiz.models;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -23,9 +19,5 @@ public class Question {
 
     private String title;
     private String type;
-
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
-    List<Answer> answers = new ArrayList<>();
-
 
 }
